37605bcf98d08fe7d46423c7e88608324f9b0738
uwu <146203643+alapkin1@users.noreply.github.com>
2023-10-05 22:13:11 +0300

    Update pinpong.py

--- python/pinpong.py
+++ python/pinpong.py
@@ -9,10 +9,11 @@ background = transform.scale(image.load('fon.jpg'), window_size)
 
 
 class GameSprite(sprite.Sprite): 
-    def __init__(self, player_image,x_size , y_size, x_cor, y_cor, speed):
+    def __init__(self, player_image,x_size , y_size, x_cor, y_cor, speed_x, speed_y):
         super().__init__()
         self.image = transform.scale(image.load(player_image),(x_size, y_size))
-        self.speed = speed
+        self.speed_x = speed_x
+        self.speed_y = speed_y
         self.rect = self.image.get_rect()
         self.rect.x = x_cor
         self.rect.y = y_cor
@@ -25,28 +26,42 @@ class Rocket(GameSprite):
     def update(self):
         keys_pressed = key.get_pressed()   
         if keys_pressed[K_w] and self.rect.y > 0:
-            self.rect.y -= self.speed 
+            self.rect.y -= self.speed_y
         if keys_pressed[K_s] and self.rect.y < 635:
-            self.rect.y += self.speed 
+            self.rect.y += self.speed_y
 
 class Rocket2(GameSprite):
     def update(self):
         keys_pressed = key.get_pressed()   
         if keys_pressed[K_UP] and self.rect.y > 0:
-            self.rect.y -= self.speed 
+            self.rect.y -= self.speed_y 
         if keys_pressed[K_DOWN] and self.rect.y < 635:
-            self.rect.y += self.speed 
+            self.rect.y += self.speed_y
 
 class Ball(GameSprite):
     def update(self):
-        self.rect.x += self.speed
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        if self.rect.y <= 0:
+            self.speed_y *= -1
+        if self.rect.y >= 460:
+            self.speed_y *= -1
+        if ball.rect.colliderect(player.rect) or ball.rect.colliderect(player2.rect):
+            self.speed_x *= -1
+            self.speed_y *= 1
 
 
+player = Rocket('rocket.png', 40, 150, 0, 350, 0, 5)
+player2 = Rocket2('rocket.png', 40, 150, 660, 350, 0, 5)
+ball = Ball('ball.jpg', 40, 40, 300, 300, 3, 3)
 
-player = Rocket('rocket.png', 40, 150, 0, 350, 5)
-player2 = Rocket2('rocket.png', 40, 150, 660, 350, 5)
-ball = Ball('ball.jpg', 40, 40, 300, 300, 1)
+font.init()  
+form = font.SysFont('Arial', 30)
 
+score_l = 0
+score_r = 0
+
+finish = True
 game = True
 FPS = 60
 clock = time.Clock()
@@ -55,15 +70,28 @@ while game:
         if e.type == QUIT:
             game = False
 
+    if finish == True:
+        window.blit(background, (0,0))  
 
-    window.blit(background, (0,0))    
-    player.update()
-    player.reset()
-    player2.update()
-    player2.reset()
-    ball.update()
-    ball.reset()
-    
+        player.update()
+        player.reset()
+        player2.update()
+        player2.reset()
+        ball.update()
+        ball.reset()
+
+        if ball.rect.x <= -80:
+            score_l += 1
+            ball.rect.x = 350
+        elif ball.rect.x >= 780:
+            score_r += 1
+            ball.rect.x = 350 
+
+        pl1_score = form.render('Player1:' + str(score_r), True, [0, 0, 0])
+        pl2_score = form.render('Player2:' + str(score_l), True, [0, 0, 0])
+
+        window.blit(pl1_score, (0, 25))
+        window.blit(pl2_score, (0, 50))
 
 
     display.update()
